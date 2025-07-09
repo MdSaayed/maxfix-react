eef04b56537f9245df2b1136eb43b6ab2fd30012
MdSaayed <mdsayed.contact@gmail.com>
2025-07-09 14:08:58 +0600

    Created Footer

--- src/components/homes/home-1/index.jsx
+++ src/components/homes/home-1/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderOne from '../../../layouts/headers/HeaderOne';
+import FooterOne from '../../../layouts/footers/FooterOne';
 
 const HomeOne = () => {
     return (
          <>
             <HeaderOne />
+            <FooterOne />
          </>
     );
 };
--- src/components/homes/home-2/index.jsx
+++ src/components/homes/home-2/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeaderTwo from '../../../layouts/headers/HeaderTwo';
+import FooterTwo from '../../../layouts/footers/footerTwo';
 
 const HomeTwo = () => {
     return (
         <>
             <HeaderTwo />
+            <FooterTwo />
         </>
     );
 };
